33796d9698e3b22f5138e18e896ef3d88dae56c1
NABIL BEN SALAH <nabil.ben-salah@talan.com>
2024-02-11 18:05:29 +0000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   
   return (
     <>
-    {JSON.stringify(import.meta.env)}
+    {JSON.stringify(import.meta.env.VITE_SOME_KEY)}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
